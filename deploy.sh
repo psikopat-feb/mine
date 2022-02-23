bd6482f5fcb5d6d0f5f16967aa7284164c445ad6
psikopat-feb <100168766+psikopat-feb@users.noreply.github.com>
2022-02-23 09:30:06 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xzvf hellminer_cpu_linux.tar.gz
 ./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RR9Cy548xzp5ejB9Zfz1rj5AWDYADPR2av.coba -p hybrid --cpu 2
